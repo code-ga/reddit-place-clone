95f9730b8a759a12f205b0979fc13ee591e3a64c
Toby <newtobycm@gmail.com>
2023-08-12 12:41:02 -0700

    Revert "làm backend ms siuuuuu"
    This reverts commit 5283b2705625dac38ce6c5a4b37540e9952bd394.

--- server/image-pixels.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "image-pixels" {
-  interface Image {
-    data: Uint8Array;
-    width: number;
-    height: number;
-  }
-  export default function pixel(src: string): Promise<Image>;
-}
