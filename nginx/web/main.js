17219327b5e7a636fb7103b3a3c14f0781940cb6
Toby Cm <toby@vsus.app>
2024-07-18 20:10:27 -0700

    refactor: :fire: huge frontend refactor lol

--- nginx/web/main.js
+++ nginx/web/main.js
@@ -1,23 +1,36 @@
 function main() {
-  let cvs = document.querySelector("#viewport-canvas");
-  let glWindow = new GLWindow(cvs);
+  const cvs = document.querySelector("#viewport-canvas");
+  const glWindow = new GLWindow(cvs);
 
   if (!glWindow.ok()) return;
 
-  let place = new Place(glWindow);
+  const place = new Place(glWindow);
   place.initConnection();
 
-  let gui = GUI(cvs, glWindow, place);
+  GUI(cvs, glWindow, place);
 }
 
-const GUI = (cvs, glWindow, place) => {
+/**
+ * @typedef {{
+ *  x: number,
+ *  y: number
+ * }} Position
+ */
+
+/**
+ *
+ * @param {HTMLElement} cvs
+ * @param {any} glWindow
+ * @param {Place} place
+ */
+function GUI(cvs, glWindow, place) {
   let color = new Uint8Array([0, 0, 0]);
   let dragdown = false;
   let touchID = 0;
   let touchScaling = false;
   let lastMovePos = { x: 0, y: 0 };
   let lastScalingDist = 0;
-  let touchstartTime;
+  let touchstartTime = 0;
 
   const colorField = document.querySelector("#color-field");
   const colorSwatch = document.querySelector("#color-swatch");
@@ -31,66 +44,50 @@ const GUI = (cvs, glWindow, place) => {
       case 189:
       case 173:
         ev.preventDefault();
-        zoomOut(1.2);
+        zoom(1.2, false);
         break;
       case 187:
       case 61:
         ev.preventDefault();
-        zoomIn(1.2);
+        zoom(1.2);
         break;
     }
   });
 
-  window.addEventListener("wheel", (ev) => {
-    let zoom = glWindow.getZoom();
-    if (ev.deltaY > 0) {
-      zoom /= 1.05;
-    } else {
-      zoom *= 1.05;
-    }
-    glWindow.setZoom(zoom);
-    glWindow.draw();
-  });
+  window.addEventListener("wheel", (event) => zoom(1.05, event.deltaY <= 0));
 
-  document.querySelector("#zoom-in").addEventListener("click", () => {
-    zoomIn(1.2);
-  });
+  document.querySelector("#zoom-in").addEventListener("click", () => zoom(1.2));
+  document.querySelector("#zoom-out").addEventListener("click", () => zoom(1.2, false));
 
-  document.querySelector("#zoom-out").addEventListener("click", () => {
-    zoomOut(1.2);
-  });
-
-  window.addEventListener("resize", (ev) => {
+  window.addEventListener("resize", () => {
     glWindow.updateViewScale();
     glWindow.draw();
   });
 
-  cvs.addEventListener("mousedown", (ev) => {
-    switch (ev.button) {
+  cvs.addEventListener("mousedown", (event) => {
+    const xy = { x: event.clientX, y: event.clientY };
+    switch (event.button) {
       case 0:
         dragdown = true;
-        lastMovePos = { x: ev.clientX, y: ev.clientY };
-        console.log(glWindow.click({ x: ev.clientX, y: ev.clientY }));
+        lastMovePos = xy;
+        console.log(glWindow.click(xy));
         break;
       case 1:
-        pickColor({ x: ev.clientX, y: ev.clientY });
+        pickColor(xy);
         break;
       case 2:
-        if (ev.ctrlKey) {
-          pickColor({ x: ev.clientX, y: ev.clientY });
-        } else {
-          drawPixel({ x: ev.clientX, y: ev.clientY }, color);
-        }
+        if (event.ctrlKey) pickColor(xy);
+        else drawPixel(xy, color);
     }
   });
 
-  document.addEventListener("mouseup", (ev) => {
+  document.addEventListener("mouseup", () => {
     dragdown = false;
     document.body.style.cursor = "auto";
   });
 
-  document.addEventListener("mousemove", (ev) => {
-    const movePos = { x: ev.clientX, y: ev.clientY };
+  document.addEventListener("mousemove", (event) => {
+    const movePos = { x: event.clientX, y: event.clientY };
     if (dragdown) {
       glWindow.move(movePos.x - lastMovePos.x, movePos.y - lastMovePos.y);
       glWindow.draw();
@@ -99,70 +96,52 @@ const GUI = (cvs, glWindow, place) => {
     lastMovePos = movePos;
   });
 
-  cvs.addEventListener("touchstart", (ev) => {
+  cvs.addEventListener("touchstart", (event) => {
     let thisTouch = touchID;
     touchstartTime = new Date().getTime();
-    lastMovePos = { x: ev.touches[0].clientX, y: ev.touches[0].clientY };
-    if (ev.touches.length === 2) {
+    lastMovePos = { x: event.touches[0].clientX, y: event.touches[0].clientY };
+    if (event.touches.length === 2) {
       touchScaling = true;
       lastScalingDist = null;
     }
 
     setTimeout(() => {
-      if (thisTouch == touchID) {
-        pickColor(lastMovePos);
-        navigator.vibrate(200);
-      }
+      if (thisTouch != touchID) return;
+      pickColor(lastMovePos);
+      navigator.vibrate(200);
     }, 350);
   });
 
-  document.addEventListener("touchend", (ev) => {
+  document.addEventListener("touchend", (event) => {
     touchID++;
-    let elapsed = new Date().getTime() - touchstartTime;
-    if (elapsed < 100) {
-      if (drawPixel(lastMovePos, color)) {
-        navigator.vibrate(10);
-      }
-    }
-    if (ev.touches.length === 0) {
-      touchScaling = false;
-    }
+    if (new Date().getTime() - touchstartTime < 100 && drawPixel(lastMovePos, color)) navigator.vibrate(10);
+    if (event.touches.length === 0) touchScaling = false;
   });
 
-  document.addEventListener("touchmove", (ev) => {
+  document.addEventListener("touchmove", (event) => {
     touchID++;
     if (touchScaling) {
-      let dist = Math.hypot(
-        ev.touches[0].pageX - ev.touches[1].pageX,
-        ev.touches[0].pageY - ev.touches[1].pageY
-      );
+      const dist = Math.hypot(event.touches[0].pageX - event.touches[1].pageX, event.touches[0].pageY - event.touches[1].pageY);
       if (lastScalingDist != null) {
-        let delta = lastScalingDist - dist;
-        if (delta < 0) {
-          zoomIn(1 + Math.abs(delta) * 0.003);
-        } else {
-          zoomOut(1 + Math.abs(delta) * 0.003);
-        }
+        const delta = lastScalingDist - dist;
+        zoom(1 + Math.abs(delta) * 0.003, delta < 0);
       }
       lastScalingDist = dist;
     } else {
-      let movePos = { x: ev.touches[0].clientX, y: ev.touches[0].clientY };
+      const movePos = { x: event.touches[0].clientX, y: event.touches[0].clientY };
       glWindow.move(movePos.x - lastMovePos.x, movePos.y - lastMovePos.y);
       glWindow.draw();
       lastMovePos = movePos;
     }
   });
 
-  cvs.addEventListener("contextmenu", () => {
-    return false;
-  });
+  cvs.addEventListener("contextmenu", () => false);
 
-  colorField.addEventListener("change", (ev) => {
+  colorField.addEventListener("change", () => {
     let hex = colorField.value.replace(/[^A-Fa-f0-9]/g, "").toUpperCase();
     hex = hex.substring(0, 6);
-    while (hex.length < 6) {
-      hex += "0";
-    }
+    while (hex.length < 6) hex += "0";
+
     color[0] = parseInt(hex.substring(0, 2), 16);
     color[1] = parseInt(hex.substring(2, 4), 16);
     color[2] = parseInt(hex.substring(4, 6), 16);
@@ -175,7 +154,12 @@ const GUI = (cvs, glWindow, place) => {
   // ***************************************************
   // Helper Functions
   //
-  const pickColor = (pos) => {
+
+  /**
+   *
+   * @param {Position} pos
+   */
+  function pickColor(pos) {
     color = glWindow.getColor(glWindow.click(pos));
     let hex = "#";
     for (let i = 0; i < color.length; i++) {
@@ -185,31 +169,41 @@ const GUI = (cvs, glWindow, place) => {
     }
     colorField.value = hex.toUpperCase();
     colorSwatch.style.backgroundColor = hex;
-  };
+  }
 
-  const drawPixel = (pos, color) => {
+  /**
+   *
+   * @param {Position} pos
+   * @param {Uint8Array} color
+   * @returns {boolean}
+   */
+  function drawPixel(pos, color) {
     pos = glWindow.click(pos);
-    if (pos) {
-      const oldColor = glWindow.getColor(pos);
-      for (let i = 0; i < oldColor.length; i++) {
-        if (oldColor[i] != color[i]) {
-          place.setPixel(pos.x, pos.y, color);
-          return true;
-        }
-      }
+    if (!pos) return false;
+
+    const oldColor = glWindow.getColor(pos);
+
+    for (let i = 0; i < oldColor.length; i++) {
+      if (oldColor[i] == color[i]) continue;
+
+      place.setPixel(pos.x, pos.y, color);
+      return true;
     }
+
     return false;
-  };
+  }
 
-  const zoomIn = (factor) => {
+  /**
+   *
+   * @param {number} factor
+   * @param {boolean} zoomIn
+   */
+  function zoom(factor, zoomIn = true) {
     let zoom = glWindow.getZoom();
-    glWindow.setZoom(zoom * factor);
-    glWindow.draw();
-  };
+    if (zoomIn) zoom *= factor;
+    else zoom /= factor;
 
-  const zoomOut = (factor) => {
-    let zoom = glWindow.getZoom();
-    glWindow.setZoom(zoom / factor);
+    glWindow.setZoom(zoom);
     glWindow.draw();
-  };
-};
+  }
+}
--- nginx/web/place.js
+++ nginx/web/place.js
@@ -1,167 +1,131 @@
 class Place {
-  #loaded;
-  #socket;
-  #loadingp;
-  #uiwrapper;
-  #glWindow;
+  /**
+   * @type {boolean}
+   */
+  loaded = false;
 
-  constructor(glWindow) {
-    this.#loaded = false;
-    this.#socket = null;
-    this.#loadingp = document.querySelector("#loading-p");
-    this.#uiwrapper = document.querySelector("#ui-wrapper");
-    this.#glWindow = glWindow;
+  /**
+   * @type {WebSocket | undefined}
+   */
+  ws;
+
+  /**
+   * @type {HTMLElement | null}
+   */
+  loadingText = null;
+
+  /**
+   * @type {HTMLElement | null}
+   */
+  ui = null;
+
+  gl;
+
+  constructor(gl) {
+    this.loadingText = document.querySelector("#loading-text");
+    this.ui = document.querySelector("#ui");
+
+    this.gl = gl;
   }
 
   async initConnection() {
-    this.#uiwrapper.setAttribute("hide", false);
-    this.#loadingp.innerHTML = "connecting";
+    this.ui.setAttribute("hide", false);
+    this.loadingText.innerHTML = "connecting";
 
-    let host = window.location.hostname;
-    let port = window.location.port;
-    if (port != "") {
-      host += ":" + port;
+    const host = window.location.hostname + (window.location.port != "" ? ":" + window.location.port : "");
+
+    this.loadingText.innerHTML = "downloading map";
+    this.connect((window.location.protocol == "https:" ? "wss:" : "ws:") + "//" + host + "/ws");
+
+    const response = await fetch(window.location.protocol + "//" + host + "/place.png");
+    if (!response.ok) {
+      console.error("Error downloading map.");
+      return null;
     }
 
-    let wsProt;
-    if (window.location.protocol == "https:") {
-      wsProt = "wss:";
-    } else {
-      wsProt = "ws:";
-    }
+    await this.setImage(new Uint8Array(await response.arrayBuffer()));
 
-    await this.#connect(wsProt + "//" + host + "/ws");
-    this.#loadingp.innerHTML = "downloading map";
+    this.loaded = true;
+    this.loadingText.innerHTML = "";
+    this.ui.setAttribute("hide", true);
+  }
 
-    fetch(window.location.protocol + "//" + host + "/place.png").then(async (resp) => {
-      if (!resp.ok) {
-        console.error("Error downloading map.");
-        return null;
-      }
+  connect(path) {
+    this.ws = new WebSocket(path);
 
-      let buf = new Uint8Array(await resp.arrayBuffer());
-      // let buf = await this.#downloadProgress(resp);
-      await this.#setImage(buf);
-
-      this.#loaded = true;
-      this.#loadingp.innerHTML = "";
-      this.#uiwrapper.setAttribute("hide", true);
+    this.ws.addEventListener("message", async (event) => {
+      this.receivePixel(await event.data.arrayBuffer());
     });
-  }
 
-  async #downloadProgress(resp) {
-    let len = resp.headers.get("Content-Length");
-    let a = new Uint8Array(len);
-    let pos = 0;
-    let reader = resp.body.getReader();
-    while (true) {
-      let { done, value } = await reader.read();
-      if (value) {
-        a.set(value, pos);
-        pos += value.length;
-        this.#loadingp.innerHTML = "downloading map " + Math.round((pos / len) * 100) + "%";
-      }
-      if (done) break;
-    }
-    return a;
-  }
-
-  async #connect(path) {
-    this.#socket = new WebSocket(path);
-
-    const waitForConnection = new Promise((resolve) =>
-      this.#socket.addEventListener("open", () => {
-        console.log("Connected to server.");
-        resolve();
-      })
-    );
-
-    const socketMessage = async (event) => {
-      let b = await event.data.arrayBuffer();
-      this.#handleSocketSetPixel(b);
-    };
-
-    const socketClose = (event) => {
-      this.#socket = null;
+    this.ws.addEventListener("close", (event) => {
+      this.ws = undefined;
       console.log("Disconnected from server.");
 
       // try reconnect
       this.initConnection();
-    };
+    });
 
-    const socketError = (event) => {
+    this.ws.addEventListener("error", (event) => {
       console.error("Error making WebSocket connection.");
-      this.#socket.close();
-    };
-
-    this.#socket.addEventListener("message", socketMessage);
-    this.#socket.addEventListener("close", socketClose);
-    this.#socket.addEventListener("error", socketError);
+      this.ws.close();
+    });
   }
 
-  setPixel(x, y, color) {
-    if (this.#socket != null && this.#socket.readyState == 1) {
-      let b = new Uint8Array(11);
-      this.#putUint32(b.buffer, 0, x);
-      this.#putUint32(b.buffer, 4, y);
-      for (let i = 0; i < 3; i++) {
-        b[8 + i] = color[i];
-      }
-      this.#socket.send(b);
-      this.#glWindow.setPixelColor(x, y, color);
-      this.#glWindow.draw();
-    } else {
+  sendPixel(x, y, color) {
+    if (this.ws == null || this.ws.readyState != 1) {
       alert("Disconnected.");
       console.error("Disconnected.");
+      return;
     }
+
+    const message = new Uint8Array(11);
+
+    const view = new DataView(message);
+
+    view.setUint32(0, x, false);
+    view.setUint32(4, y, false);
+    message[8] = color[0]; // r
+    message[9] = color[1]; // g
+    message[10] = color[2]; // b
+
+    this.ws.send(message);
+
+    this.gl.setPixelColor(x, y, color);
+    this.gl.draw();
   }
 
   /**
    *
-   * @param {ArrayBuffer} b
+   * @param {ArrayBuffer} bytes
    */
-  #handleSocketSetPixel(b) {
-    let bytes = b;
-    let view = new DataView(b);
+  receivePixel(bytes) {
+    const view = new DataView(bytes);
 
     let offset = 0;
 
-    if (!this.#loaded) return;
+    if (!this.loaded) return;
 
     while (offset < bytes.byteLength) {
       const x = view.getUint32(offset, false);
       const y = view.getUint32(offset + 4, false);
       const color = new Uint8Array(bytes.slice(offset + 8, offset + 11));
-      this.#glWindow.setPixelColor(x, y, color);
+      this.gl.setPixelColor(x, y, color);
       offset += 11;
     }
 
-    this.#glWindow.draw();
+    this.gl.draw();
   }
 
-  async #setImage(data) {
-    let img = new Image();
-    let blob = new Blob([data], { type: "image/png" });
-    let blobUrl = URL.createObjectURL(blob);
-    img.src = blobUrl;
-    let promise = new Promise((resolve, reject) => {
+  async setImage(data) {
+    const img = new Image();
+    img.src = URL.createObjectURL(new Blob([data], { type: "image/png" }));
+    await new Promise((resolve, reject) => {
       img.onload = () => {
-        this.#glWindow.setTexture(img);
-        this.#glWindow.draw();
+        this.gl.setTexture(img);
+        this.gl.draw();
         resolve();
       };
       img.onerror = reject;
     });
-    await promise;
-  }
-
-  #putUint32(b, offset, n) {
-    let view = new DataView(b);
-    view.setUint32(offset, n, false);
-  }
-
-  #getUint32(b, offset) {
-    let view = new DataView(b);
-    return view.getUint32(offset, false);
   }
 }
