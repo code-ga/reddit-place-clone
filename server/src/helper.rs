a675c85bb781592e9bcb32528c885e7486e564f7
thedtvn <duongtuan30306@gmail.com>
2025-06-25 11:58:07 +0700

    Refactor dependencies and improve image handling in StadeData

--- server/src/helper.rs
+++ server/src/helper.rs
@@ -1,9 +1,8 @@
 use std::{path::PathBuf, sync::Arc};
-
-use async_channel::Receiver;
 use clap::Parser;
-use image::RgbImage;
-use tokio::sync::{mpsc::UnboundedSender, RwLock};
+use dashmap::DashMap;
+use image::Rgb;
+use tokio::sync::broadcast::{Receiver, Sender};
 
 #[derive(Parser, Debug, Clone)]
 pub struct ConfigArgs {
@@ -25,12 +24,12 @@ pub struct ConfigArgs {
 pub struct Point {
     pub x: u32,
     pub y: u32,
-    pub color: image::Rgb<u8>
+    pub color: [u8; 3]
 }
 
 impl Point {
     pub fn new(x: u32, y: u32, r: u8, g: u8, b: u8) -> Self {
-        let color = image::Rgb([r, g, b]);
+        let color = [r, g, b];
         Self { x, y, color }
     }
     
@@ -40,9 +39,9 @@ impl Point {
         result.extend(&x);
         let y = self.y.to_be_bytes();
         result.extend(&y);
-        let r = self.color.0[0];
-        let g = self.color.0[1];
-        let b = self.color.0[2];
+        let r = self.color[0];
+        let g = self.color[1];
+        let b = self.color[2];
         result.extend(&[r,g,b]);
         result
     }
@@ -59,13 +58,74 @@ impl Point {
 
 #[derive(Debug, Clone)]
 pub struct StadeData {
-    pub image: Arc<RwLock<RgbImage>>,
-    pub sender: UnboundedSender<Point>,
-    pub receiver: Receiver<Point>
+    width: u32,
+    height: u32,
+    image_dashmap: Arc<DashMap<u32, [u8; 3]>>,
+    global_receiver: Arc<Receiver<Point>>,
+    global_notifier: Sender<Point>
 }
 
 impl StadeData {
-    pub fn new(image: Arc<RwLock<RgbImage>>, sender: UnboundedSender<Point>, receiver: Receiver<Point>) -> Self {
-        Self { image, sender, receiver}
+    pub fn new(width: u32, height: u32) -> Self {
+        let (global_notifier, global_receiver) = tokio::sync::broadcast::channel::<Point>(10);
+        let image_dashmap = Arc::new(DashMap::new());
+        Self { image_dashmap, global_receiver: Arc::new(global_receiver), global_notifier, width, height }
     }
+
+    fn coordinates_to_index(&self, x: u32, y: u32) -> u32 {
+        let width = self.image_dashmap.len() as u32;
+        y * width + x
+    }
+
+    pub async fn set_pixel(&self, raw: &[u8]) {
+        let point = Point::from_byte(raw);
+        self.global_notifier.send(point.clone()).unwrap();
+        if point.color == [255; 3] {
+            self.image_dashmap.remove(&self.coordinates_to_index(point.x, point.y));
+            return;
+        }
+        self.image_dashmap.insert(self.coordinates_to_index(point.x, point.y), point.color);
+    }
+
+    pub fn get_image(&self) -> image::RgbImage {
+        let mut image_raw = vec![255u8; (self.width * self.height * 3) as usize];
+        
+        for entry in self.image_dashmap.iter() {
+            let index = *entry.key();
+            let x = index % self.width;
+            let y = index / self.width;
+            let pixel_index = ((y * self.width + x) * 3) as usize;
+            let color = entry.value();
+            
+            image_raw[pixel_index] = color[0];
+            image_raw[pixel_index + 1] = color[1];
+            image_raw[pixel_index + 2] = color[2];
+        }
+        
+        let image = image::RgbImage::from_raw(
+            self.width,
+            self.height,
+            image_raw
+        ).expect("Failed to create image buffer");
+        image
+    }
+
+    pub fn listen(&self) -> Receiver<Point> {
+        self.global_receiver.resubscribe()
+    }
+
+    pub fn load_from_old_image(&self, image: &image::RgbImage) {
+        for (x, y, pixel) in image.enumerate_pixels() {
+            if x >= self.width || y >= self.height {
+                continue; // Skip pixels outside the bounds
+            } else if *pixel == Rgb([255, 255, 255]) {
+                continue; // Skip white pixels
+            }
+            self.image_dashmap.insert(
+                self.coordinates_to_index(x, y),
+                [pixel[0], pixel[1], pixel[2]]
+            );
+        }
+    }
+
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,111 +1,119 @@
 mod helper;
+
 use std::path::PathBuf;
 use std::sync::Arc;
-use axum::extract::ws::Message;
-use futures_util::{SinkExt as _, StreamExt};
+
 use axum::body::Body;
-use axum::extract::{State, WebSocketUpgrade, ws::WebSocket};
+use axum::extract::ws::Message;
+use axum::extract::{ws::WebSocket, State, WebSocketUpgrade};
 use axum::http::Response;
 use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::Router;
 use clap::Parser as _;
-use helper::{ConfigArgs, Point, StadeData};
-use image::RgbImage;
-use image::ImageReader;
+use futures_util::{SinkExt as _, StreamExt};
+use image::{ImageReader, RgbImage};
 use lazy_static::lazy_static;
-use tokio::sync::RwLock;
+
+use helper::{ConfigArgs, StadeData};
 
 lazy_static! {
-    static ref Config: ConfigArgs = ConfigArgs::parse();    
+    static ref Config: ConfigArgs = ConfigArgs::parse();
 }
 
 #[tokio::main]
 async fn main() {
     println!("loading image...");
+
     let img_w = Config.width.unwrap_or(1000);
     let img_h = Config.height.unwrap_or(1000);
-    let image = Arc::new(RwLock::new(RgbImage::new(img_w, img_h)));
-    let mut lock_w = image.write().await;
-    for x in 0..img_w {
-        for y in 0..img_h {
-            lock_w.put_pixel(x, y, image::Rgb([255, 255, 255]));
-        }
-    }
-    drop(lock_w);
-    let img_path = Config.save_location.clone().unwrap_or(PathBuf::from("place.png"));
+    let s_data = StadeData::new(img_w, img_h);
+
+    let img_path = Config
+        .save_location
+        .clone()
+        .unwrap_or(PathBuf::from("place.png"));
     if !img_path.to_str().unwrap().ends_with(".png") {
         panic!("image path must end with .png");
-    }
-    if img_path.exists() {
-        let result = load_old_image(img_path.clone(), image.clone()).await;
-        if result.is_err() {
-            println!("failed to load old image remove image to save new image");
-            if img_path.is_file() {
-                let _ = std::fs::remove_file(img_path.clone());
-            } else {
-                panic!("is not a file");
+    } else if img_path.exists() {
+        println!("loading old image from {}", img_path.to_str().unwrap());
+        let img = open_img(img_path.clone()).await;
+        match img {
+            Ok(img) => {
+                s_data.load_from_old_image(&img);
+                println!("image loaded successfully");
+            }
+            Err(e) => {
+                eprintln!("error loading image: {}", e);
+                println!("image exists but could not be loaded, creating new one");
             }
         }
+    } else {
+        println!("image not found, created new one");
     }
-    println!("completed loading image");
-    let img_save_clone = image.clone();
-    let img_path_cl = img_path.clone();
+
+    let save_interval = Config.save_interval.unwrap_or(120);
+    let s_data_cp = s_data.clone();
+    let img_path_cp = img_path.clone();
     tokio::spawn(async move {
-        println!("start save image every {} seconds", Config.save_interval.unwrap_or(120));
         loop {
-            tokio::time::sleep(std::time::Duration::from_secs(Config.save_interval.unwrap_or(120))).await;
-            println!("scheduled start save image");
-            let img = img_save_clone.read().await.to_owned();
-            if Config.save_all_images.unwrap_or_default() {
+            tokio::time::sleep(std::time::Duration::from_secs(save_interval)).await;
+            if Config.save_all_images.unwrap_or(false) {
                 save_old_image().await;
             }
-            let _ = img.save(img_path_cl.clone());
-            let file_load_r = open_img(img_path_cl.clone()).await;
-            if file_load_r.is_err() { 
-                println!("failed to load image");
+            let status = s_data_cp.get_image().save(
+                img_path_cp.clone(),
+            );
+            match status {
+                Ok(_) => println!("image saved successfully"),
+                Err(e) => eprintln!("error saving image: {}", e),
             }
-            println!("scheduled save image completed");
         }
     });
-    let (tx_set, mut rx_set) = tokio::sync::mpsc::unbounded_channel::<Point>();
-    let (tx_noti, rx_noti) = async_channel::unbounded::<Point>();
-    let image_cl = image.clone();
-    tokio::spawn(async move {
-        println!("start set pixel event listener");
-        while let Some(ptr) = rx_set.recv().await {
-            let img_lock = image_cl.read().await.to_owned();
-            if ptr.x >= img_lock.width() || ptr.y >= img_lock.height() { continue; }
-            let _ = tx_noti.send(ptr.clone()).await;
-            image_cl.write().await.put_pixel(ptr.x, ptr.y, ptr.color);
-        }
-    });
-    let s_data = StadeData::new(image.clone(), tx_set, rx_noti);
+
     let app = Router::new()
-                        .route("/ws", get(ws_hendler))
-                        .route("/place.png", get(place_image))
-                        .with_state(s_data);
+        .route("/ws", get(ws_hendler))
+        .route("/place.png", get(place_image))
+        .with_state(s_data.clone());
+
     let address = Config.address.clone().unwrap_or("0.0.0.0:8080".to_string());
-    let listener = tokio::net::TcpListener::bind(address.clone()).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(address.clone())
+        .await
+        .unwrap();
     println!("Start server at {}", address);
+
     let path_copy = img_path.clone();
-    let _ = axum::serve(listener, app).with_graceful_shutdown(async move { 
-        tokio::signal::ctrl_c().await.unwrap();
-        println!("save image before exit");
-        let img = image.read().await.to_owned();
-        if Config.save_all_images.unwrap_or_default() {
-            save_old_image().await;
-        }
-        let _ = img.save(path_copy);
-        println!("save image completed");
-        std::process::exit(0);
-    }).await;
+    let s_data_shutdown = s_data.clone();
+
+    let _ = axum::serve(listener, app)
+        .with_graceful_shutdown(async move {
+            tokio::signal::ctrl_c().await.unwrap();
+            println!("save image before exit");
+            if Config.save_all_images.unwrap_or(false) {
+                save_old_image().await;
+            }
+            let status = s_data_shutdown.get_image().save(path_copy);
+            match status {
+                Ok(_) => println!("image saved successfully before exit"),
+                Err(e) => eprintln!("error saving image before exit: {}", e),
+            }
+        })
+        .await;
 }
 
 async fn save_old_image() {
-    let namd_cl = Config.save_location.clone().unwrap_or(PathBuf::from("place.png")).clone();
+    let namd_cl = Config
+        .save_location
+        .clone()
+        .unwrap_or(PathBuf::from("place.png"))
+        .clone();
     let mut new_old_file_name = namd_cl.to_str().unwrap().split(".").collect::<Vec<&str>>();
-    let unix_time = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs().to_string();
+    let unix_time = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_secs()
+        .to_string();
+
     new_old_file_name.insert(new_old_file_name.len() - 2, unix_time.as_str());
     println!("{}", new_old_file_name.join("."));
     let _ = tokio::fs::rename(namd_cl.clone(), new_old_file_name.join(".")).await;
@@ -118,46 +126,34 @@ async fn open_img(path: PathBuf) -> Result<RgbImage, Box<dyn std::error::Error>>
     Ok(rgb_img)
 }
 
-async fn load_old_image(old_image_path: PathBuf, image: Arc<RwLock<RgbImage>>) -> Result<(), Box<dyn std::error::Error>> {
-    let image_cl = image.read().await.to_owned();
-    let rgb_img = open_img(old_image_path).await?;
-    let img_w = [image_cl.width(), rgb_img.width()].iter().min().unwrap().to_owned();
-    let img_h = [image_cl.height(), rgb_img.height()].iter().min().unwrap().to_owned();
-    let mut lock_w = image.write().await;
-    for x in 0..img_w {
-        for y in 0..img_h {
-            let pixel = rgb_img.get_pixel(x, y);
-            lock_w.put_pixel(x, y, *pixel);
-        }
-    }
-    Ok(())
-}
-
-
-
-async fn ws_hendler(State(stade_data): State<StadeData>, ws: WebSocketUpgrade) -> impl IntoResponse {
+async fn ws_hendler(
+    State(stade_data): State<StadeData>,
+    ws: WebSocketUpgrade,
+) -> impl IntoResponse {
     ws.on_upgrade(move |socket| handle_socket(socket, stade_data))
 }
 
 async fn handle_socket(socket: WebSocket, stade_data: StadeData) {
-
     let (mut sender, mut receiver) = socket.split();
     let notifyer = Arc::new(tokio::sync::Notify::new());
 
     let (tx_sender, mut rx_sender) = tokio::sync::mpsc::unbounded_channel::<Message>();
     let notifyer_cp = notifyer.clone();
+
     let ws_sender = tokio::spawn(async move {
         while let Some(msg) = rx_sender.recv().await {
             let _ = sender.send(msg).await;
         }
     });
+
     let tx_sender_cl = tx_sender.clone();
+    let stade_data_clone = stade_data.clone();
     let ws_receiver = tokio::spawn(async move {
         while let Some(Ok(msg)) = receiver.next().await {
             match msg.clone() {
                 Message::Binary(data) => {
-                    let _ = stade_data.sender.send(Point::from_byte(&data));
-                },
+                    let _ = stade_data_clone.set_pixel(&data).await;
+                }
                 Message::Ping(data) => {
                     let _ = tx_sender_cl.send(Message::Pong(data));
                 }
@@ -175,20 +171,25 @@ async fn handle_socket(socket: WebSocket, stade_data: StadeData) {
             }
         }
     });
+
     let tx_sender_cl = tx_sender.clone();
     let sync_data = tokio::spawn(async move {
-        while let Ok(point) = stade_data.receiver.recv().await {
+        while let Ok(point) = stade_data.listen().recv().await {
             let _ = tx_sender_cl.send(Message::Binary(point.to_byte()));
         }
     });
+
     let tx_sender_cl = tx_sender.clone();
     let send_ping = tokio::spawn(async move {
         loop {
             tokio::time::sleep(std::time::Duration::from_secs(30)).await;
             let r_send = tx_sender_cl.send(Message::Ping(vec![]));
-            if r_send.is_err() { break; }
+            if r_send.is_err() {
+                break;
+            }
         }
     });
+
     notifyer.notified().await;
     ws_receiver.abort();
     ws_sender.abort();
@@ -198,14 +199,15 @@ async fn handle_socket(socket: WebSocket, stade_data: StadeData) {
 }
 
 async fn place_image(State(stade_data): State<StadeData>) -> impl IntoResponse {
-    let img = stade_data.image.read().await.to_owned();
+    let img = stade_data.get_image();
     let mut crusor = std::io::Cursor::new(Vec::new());
     let _ = img.write_to(&mut crusor, image::ImageFormat::Png);
     let data = crusor.into_inner();
+
     Response::builder()
-    .header("content-type", "image/png")
-    // Cors
-    .header("Access-Control-Allow-Origin", "*")
-    .header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-    .body(Body::from(data)).unwrap()
+        .header("content-type", "image/png")
+        .header("Access-Control-Allow-Origin", "*")
+        .header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        .body(Body::from(data))
+        .unwrap()
 }
